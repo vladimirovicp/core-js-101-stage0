c177d6a8fc64c2291b807a42054536e322547bb6
vladimirovicp <vladimirovicp70@gmail.com>
2026-04-22 14:59:14 +0400

    feat: add 03

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -10,6 +10,8 @@
 /**
  * Returns an index of the specified element in array or -1 if element is not found
  *
+ * Возвращает индекс указанного элемента в массиве или -1, если элемент не найден
+ *
  * @param {array} arr
  * @param {any} value
  * @return {number}
@@ -19,12 +21,14 @@
  *    ['Array', 'Number', 'string'], 'Date'    => -1
  *    [0, 1, 2, 3, 4, 5], 5    => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  return arr.indexOf(value);
 }
 
 /**
  * Generates an array of odd numbers of the specified length
+ *
+ * Генерирует массив нечетных чисел заданной длины
  *
  * @param {number} len
  * @return {array}
@@ -34,13 +38,21 @@ function findElement(/* arr, value */) {
  *    2 => [ 1, 3 ]
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  const arr = new Array(len).fill(0);
+  let i = -1;
+  const result = arr.map(() => {
+    i += 2;
+    return i;
+  });
+  return result;
 }
 
 /**
  * Returns the doubled array - elements of the specified array
  * are repeated twice using original order
+ * Возвращает удвоенный массив — элементы указанного массива.
+ * повторяются дважды в исходном порядке
  *
  * @param {array} arr
  * @return {array}
@@ -50,12 +62,13 @@ function generateOdds(/* len */) {
  *    [0, 1, 2, 3, 4, 5] => [0, 1, 2, 3, 4, 5,   0, 1, 2, 3, 4, 5]
  *    [] => []
  */
-function doubleArray(/* arr */) {
-  throw new Error('Not implemented');
+function doubleArray(arr) {
+  return [...arr, ...arr];
 }
 
 /**
  * Returns an array of positive numbers from the specified array in original order
+ * Возвращает массив положительных чисел из указанного массива в исходном порядке.
  *
  * @param {array} arr
  * @return {array}
@@ -65,12 +78,13 @@ function doubleArray(/* arr */) {
  *    [-1, 2, -5, -4, 0] => [ 2 ]
  *    [] => []
  */
-function getArrayOfPositives(/* arr */) {
-  throw new Error('Not implemented');
+function getArrayOfPositives(arr) {
+  return arr.filter((item) => item > 0);
 }
 
 /**
  * Returns the array with strings only in the specified array (in original order)
+ * Возвращает массив со строками только в указанном массиве (в исходном порядке)
  *
  * @param {array} arr
  * @return {array}
@@ -80,14 +94,16 @@ function getArrayOfPositives(/* arr */) {
  *    [ 1, 2, 3, 4, 5 ] => []
  *    [ 'cat, 'dog', 'raccoon' ] => [ 'cat', 'dog', 'raccoon' ]
  */
-function getArrayOfStrings(/* arr */) {
-  throw new Error('Not implemented');
+function getArrayOfStrings(arr) {
+  return arr.filter((item) => typeof item === 'string');
 }
 
 /**
  * Removes falsy values from the specified array
  * Falsy values: false, null, 0, "", undefined, and NaN.
  * (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean#Description)
+ * Удаляет ложные значения из указанного массива
+ * Ложные значения: false, null, 0, «», undefined и NaN.
  *
  * @param {array} arr
  * @return {array}
@@ -97,12 +113,13 @@ function getArrayOfStrings(/* arr */) {
  *    [ 1, 2, 3, 4, 5, 'false' ]         => [ 1, 2, 3, 4, 5, 'false' ]
  *    [ false, 0, NaN, '', undefined ]   => [ ]
  */
-function removeFalsyValues(/* arr */) {
-  throw new Error('Not implemented');
+function removeFalsyValues(arr) {
+  return arr.filter((item) => item);
 }
 
 /**
  * Returns the array of uppercase strings from the specified array
+ * Возвращает массив строк в верхнем регистре из указанного массива
  *
  * @param {array} arr
  * @return {array}
@@ -112,12 +129,13 @@ function removeFalsyValues(/* arr */) {
  *    => [ 'PERMANENT-INTERNSHIP', 'GLUTINOUS-SHRIEK', 'MULTIPLICATIVE-ELEVATION' ],
  *    [ 'a', 'b', 'c', 'd', 'e', 'f', 'g' ]  => [ 'A', 'B', 'C', 'D', 'E', 'F', 'G' ]
  */
-function getUpperCaseStrings(/* arr */) {
-  throw new Error('Not implemented');
+function getUpperCaseStrings(arr) {
+  return arr.map((item) => item.toUpperCase());
 }
 
 /**
  * Returns the array of string lengths from the specified string array.
+ * Возвращает массив длин строк из указанного массива строк.
  *
  * @param {array} arr
  * @return {array}
@@ -126,12 +144,13 @@ function getUpperCaseStrings(/* arr */) {
  *    [ '', 'a', 'bc', 'def', 'ghij' ]  => [ 0, 1, 2, 3, 4 ]
  *    [ 'angular', 'react', 'ember' ] => [ 7, 5, 5 ]
  */
-function getStringsLength(/* arr */) {
-  throw new Error('Not implemented');
+function getStringsLength(arr) {
+  return arr.map((item) => item.length);
 }
 
 /**
  * Inserts the item into specified array at specified index
+ * Вставляет элемент в указанный массив по указанному индексу.
  *
  * @param {array} arr
  * @param {any} item
@@ -141,12 +160,14 @@ function getStringsLength(/* arr */) {
  *    [ 1, 3, 4, 5 ], 2, 1  => [ 1, 2, 3, 4, 5 ]
  *    [ 1, 'b', 'c'], 'x', 0  => [ 'x', 1, 'b', 'c' ]
  */
-function insertItem(/* arr, item, index */) {
-  throw new Error('Not implemented');
+function insertItem(arr, item, index) {
+  arr.splice(index, 0, item);
+  return arr;
 }
 
 /**
  * Returns the n first items of the specified array
+ * Возвращает n первых элементов указанного массива
  *
  * @param {array} arr
  * @param {number} n
@@ -155,12 +176,13 @@ function insertItem(/* arr, item, index */) {
  *    [ 1, 3, 4, 5 ], 2 => [ 1, 3 ]
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'a', 'b', 'c' ]
  */
-function getHead(/* arr, n */) {
-  throw new Error('Not implemented');
+function getHead(arr, n) {
+  return arr.slice(0, n);
 }
 
 /**
  * Returns the n last items of the specified array
+ * Возвращает n последних элементов указанного массива
  *
  * @param {array} arr
  * @param {number} n
@@ -169,13 +191,14 @@ function getHead(/* arr, n */) {
  *    [ 1, 3, 4, 5 ], 2  => [ 4, 5 ]
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'b', 'c', 'd' ]
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  return arr.slice(-1 * n);
 }
 
 /**
  * Returns CSV representation of two-dimensional numeric array.
  * https://en.wikipedia.org/wiki/Comma-separated_values
+ * Возвращает CSV-представление двумерного числового массива.
  *
  * @param {array} arr
  * @return {string}
